5b6e31f92f0747e5fc007f0dd6c38a209e2cfbc9
khoatrannfq <dangtran.khoa@nfq.com>
2024-03-17 17:38:24 +0700

    20. Valid Parentheses

---
+++ easy/20. Valid Parentheses.go
@@ -0,0 +1,37 @@
+package main
+
+func isValid(s string) bool {
+	pMap := map[string]string{
+		")": "(",
+		"]": "[",
+		"}": "{",
+	}
+
+	stack := make([]string, 0)
+
+	for _, c := range s {
+		if len(stack) == 0 {
+			stack = append(stack, string(c))
+		} else {
+			if pMap[string(c)] == stack[len(stack)-1] {
+				stack = stack[:len(stack)-1]
+			} else {
+				stack = append(stack, string(c))
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
+
+func ValidParenthesesTest() {
+	println(isValid("()"))
+	println(isValid("()[]{}"))
+	println(isValid("(]"))
+	println(isValid("([)]"))
+	println(isValid("([)]"))
+}
+
+func main() {
+	ValidParenthesesTest()
+}
